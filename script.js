9d8c9c6cb16e3abccade4e3988629c0927844c77
Leenay97 <skynekop@gmail.com>
2022-01-12 23:43:43 +0800

    выфвыф

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let nextButton = document.querySelector('.next');
+    let translateButton = document.querySelector('.translate');
     let img = document.querySelector('.japanese-text__container');
     let themes = [
         ['n1', 'n2', 'n3', 'n4', 'n5', 'n6'],
@@ -9,13 +10,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
         ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'p14', 'p15'],
         ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10', 't11', 't12', 't13', 't14', 't15', 't16', 't17', 't18', 't19', 't20']
     ]
+    let translate = '';
     nextButton.addEventListener('click', (e)=> {
         let randomTheme = themes[Math.floor(Math.random()*6)];
         console.log(randomTheme);
         let randomizer = Math.floor(Math.random()*randomTheme.length);
         console.log(randomTheme.length);
         console.log(randomizer);
-        img.innerHTML = `<img src="img/${randomTheme[randomizer]}.png"></img>`
-
+        translate = randomTheme[randomizer] + '-t';
+        console.log(translate)
+        img.innerHTML = `<img src="img/${randomTheme[randomizer]}.png"></img>`;
+    });
+    translateButton.addEventListener('click', (e)=> {
+        img.innerHTML += `<img src="img/${translate}.png"></img>`;
     });
 });
